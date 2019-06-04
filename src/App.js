0c2a4690d23c829fceec47ca94d5fc6f2b087b85
titusefferian <titus.efferian@tokopedia.com>
2019-06-04 16:47:49 +0700

    mvp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grommet, Box, TextInput, Button, Distribution, Text, Table, TableRow, TableHeader, TableCell, TableBody, Image } from 'grommet';
 import axios from 'axios';
 import MainContainer from './Components/MainContainer';
+import RecipeReviewCard from './Components/RecipeReviewCard';
+import ClipLoader from 'react-spinners/HashLoader'
 
 const fetchTokopedia = async (product) => {
   const result = await fetch('https://tokolapak-api.titusefferian.now.sh/?products=' + product)
@@ -9,10 +11,27 @@ const fetchTokopedia = async (product) => {
   return parse
 }
 
+function numberWithCommas(x) {
+  x = x.toString();
+  var pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(x))
+      x = x.replace(pattern, "$1,$2");
+  return x;
+}
+
+const numberDivider = (x) => {
+  x = x.toString()
+  let pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(x))
+    x = x.replace(pattern, "$1.$2");
+  return x;
+}
+
 function App() {
   const [text, setText] = React.useState('')
   const [result, setResult] = React.useState('')
-
+  const [loading, setLoading] = React.useState(false);
+  console.log(result);
   return (
     <>
       <Box pad="medium" direction="row" align="center" background="dark-2">
@@ -24,45 +43,54 @@ function App() {
           onChange={event => setText(event.target.value)}
           onKeyDown={async (e) => {
             if (e.key === 'Enter') {
+              setLoading(true)
               const result = await fetchTokopedia(text)
               setResult(result)
+              setLoading(false)
             }
           }}
         />
         <Button label="search" margin={{ left: '16px' }}
           primary
           onClick={async () => {
+            setLoading(true)
             const result = await fetchTokopedia(text)
             setResult(result)
+            setLoading(false)
           }} />
       </Box>
 
-      <MainContainer>
+      {
+        loading ? <div style={{height:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}><ClipLoader/></div>: 
+        <MainContainer>
         <div style={{
           flex: 1,
-          backgroundColor: 'blue',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
         }}>
           {
-            [1, 2, 3, 4, 5, 5, 6].map(x => (
-              <div style={{ width: 180, height: 200, backgroundColor: 'red', marginBottom: 16 }}></div>
+            result && result.resultTokopedia.data.searchProduct.products.map(x=>(
+              <RecipeReviewCard name={x.name} image={x.imageUrl} price={x.price} from="Tokopedia"/>
             ))
           }
         </div>
         <div style={{
           flex: 1,
-          backgroundColor: 'green',
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center',
         }}>
           {
-            [1, 2, 3, 4, 5, 5, 6].map(x => (
-              <div style={{ width: 180, height: 200, backgroundColor: 'red', marginBottom: 16 }}></div>
+            result && result.resultBukalapak.products.map(x=>(
+              <RecipeReviewCard name={x.name} image={x.small_images[0]} price={'Rp '+numberDivider(x.price)} from="Bukalapak"/>
             ))
           }
         </div>
 
       </MainContainer>
+      }
+      
     </>
   );
 }
